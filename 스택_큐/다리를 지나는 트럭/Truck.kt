41114c0815f3da89f19dbad4187aa36d5f09ebd7
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2021-12-13 13:58:05 +0900

    트럭 문제 설명 주석 추가

--- 스택_큐/다리를 지나는 트럭/Truck.kt
+++ 스택_큐/다리를 지나는 트럭/Truck.kt
@@ -9,7 +9,7 @@ fun main() {
 
 fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
     var answer = 0
-    val readyTruckQueue = LinkedList<Truck>().apply { //대기 트럭
+    val readyTruckQueue = LinkedList<Truck>().apply { //다리 건너는것을 대기하는 트럭
         truck_weights.forEach {
             offer(Truck(
                 it, bridge_length
@@ -17,12 +17,13 @@ fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
         }
     }
     val runningTruckQueue = LinkedList<Truck>() //다리를 건너는 트럭
-    val completeTruckLit = ArrayList<Truck>()
-    var totalBridgeWeight = 0
+    val completeTruckLit = ArrayList<Truck>() //다리를 다 건넌 트럭
+    var totalBridgeWeight = 0 //현재 올라와 있는 다리 무게
     while (completeTruckLit.size != truck_weights.size) {
-        //트럭이 새로 진입
-        //트럭이 움직임
-        //트럭이 다리를 다 건넘
+        //1초 후 이벤트 상황들
+        //다리 무게가 견딜 수 있다면 새로운 트럭이 진입
+        //다리 위에 있던 트럭이 한칸 움직임
+        //다리 위에 있던 트럭이 다 건넘
         if (readyTruckQueue.isNotEmpty()) {
             if (totalBridgeWeight + readyTruckQueue.peek().weight <= weight) {
                 runningTruckQueue.offer(readyTruckQueue.poll().apply {
