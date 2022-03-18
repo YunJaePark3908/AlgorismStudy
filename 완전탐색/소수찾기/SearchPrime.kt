df712cd79b6fd7b0538d770b57625f02522e15bd
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-03-18 17:27:57 +0900

    소수 찾기 정답 추가.

--- 완전탐색/소수찾기/SearchPrime.kt
+++ 완전탐색/소수찾기/SearchPrime.kt
@@ -1,23 +1,48 @@
+
 fun main() {
-    solution("0112")
+    println(solution("011"))
 }
 
 fun solution(numbers: String): Int {
-
-    val allNumberArray = permutation(numbers.map { it })
     var answer = 0
+    val numbersCharArray = numbers.map { it.toString() }
+    val combination = mutableListOf<List<String>>()
+    for (i in 1..numbersCharArray.size) {
+        combination(combination, numbersCharArray, Array<Boolean>(numbersCharArray.size) { false }, 0,  i)
+    }
+    combination.map { permutation(it) }.map {
+        it.map {
+            val sb = StringBuffer()
+            it.forEach {
+                sb.append(it)
+            }
+            sb.toString().toInt()
+        }
+    }.flatten().distinct().forEach { if (isPrime(it)) { answer++ } }
+
     return answer
 }
 
 private fun isPrime(n: Int): Boolean {
+    if (n == 0 || n == 1) return false
     var i = 2
-    while (i * i <= n) {
-        if (n % i++ == 0) return false
-    }
+    while (i * i <= n) { if (n % i++ == 0) return false }
     return true
 }
 
 fun <T> permutation(sub: List<T>, fin: List<T> = listOf()): List<List<T>> {
     return if(sub.isEmpty()) listOf(fin)
     else sub.flatMap { permutation(sub - it, fin + it) }
+}
+
+fun <T> combination(answer: MutableList<List<T>>, el: List<T>, ck: Array<Boolean>, start: Int, target: Int) {
+    if(target == 0) {
+        answer.addAll( listOf(el.filterIndexed { index, t -> ck[index] }) )
+    } else {
+        for(i in start until el.size) {
+            ck[i] = true
+            combination(answer, el, ck, i + 1, target - 1)
+            ck[i] = false
+        }
+    }
 }
