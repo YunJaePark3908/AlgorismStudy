677b5dc8e89e06e65ef9e4d0cf386309e2f5d857
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-05-19 17:38:05 +0900

    카펫 정답 코드 추가

--- 완전탐색/카펫/Carpet.kt
+++ 완전탐색/카펫/Carpet.kt
@@ -1,13 +1,21 @@
 fun main() {
-    Carpet().solution(12,3).forEach {
+    Carpet().solution(24,24).forEach {
         println(it)
     }
 }
 
 class Carpet {
     fun solution(brown: Int, yellow: Int): IntArray {
-        var answer = intArrayOf()
-        if (yellow % 2 != 0) return intArrayOf(3, yellow + 2)
-        return answer
+        val maxIdx = kotlin.math.max(brown, yellow)
+        for (height in 3 until maxIdx) {
+            for (with in height until maxIdx) {
+                val isYellow = (with - 2) * (height - 2) == yellow
+                val isBrown = (with * 2) + ((height - 2) * 2) == brown
+                if (isYellow && isBrown && with >= height) {
+                    return intArrayOf(with, height)
+                }
+            }
+        }
+        return intArrayOf()
     }
 }
