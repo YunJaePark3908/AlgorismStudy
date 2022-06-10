835b634c4be694875c0fa178d48af631f055a326
YunJaePark <54883589+YunJaePark3908@users.noreply.github.com>
2022-06-10 18:18:14 +0900

    수식 최대화 초안 파일 추가

---
+++ 카카오/수식 최대화/CalculationMax.kt
@@ -0,0 +1,11 @@
+fun main() {
+    CalculationMax().solution("100-200*300-500+20")
+}
+
+class CalculationMax {
+    fun solution(expression: String): Long {
+        val a = expression.split("-", "+")
+        var answer: Long = 0
+        return answer
+    }
+}
